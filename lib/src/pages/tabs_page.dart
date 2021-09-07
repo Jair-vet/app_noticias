09cf65c954b57c57eb03dc5b2ddeca20d7810cf9
Jair <jairaceves56@gmail.com>
2021-09-07 16:21:52 -0500

    cree el BottomNavigationBarItem con el icono person_outline

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -6,18 +6,41 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-
-          Container(
-            color: Colors.green,
-          )
-        ],
-      ),
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
    );
+  }
+}
+
+class _Navegacion extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('Para ti')),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('Para ti'))
+      ],
+    );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+
+        Container(
+          color: Colors.green,
+        )
+      ],
+    );
   }
 }
