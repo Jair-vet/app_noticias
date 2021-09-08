4e3dd16d35b971ce8e818387476344060801e149
Jair <jairaceves56@gmail.com>
2021-09-08 10:56:58 -0500

    Trabajamos con el Headlines

---
+++ lib/src/services/news_sercive.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notificaciones/src/models/news_models.dart';
+
+
+
+class NewsService with ChangeNotifier{
+
+
+  List<Article> headlines = [];
+
+  NewsService(){
+
+    this.getTopHeadlines();
+  }
+
+
+  getTopHeadlines(){
+    print('Cargando Headlines');
+  }
+
+
+
+}
