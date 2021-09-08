355f1e21bc8f4f7c77d13658a069197991225a04
Jair <jairaceves56@gmail.com>
2021-09-08 12:49:22 -0500

    Cree las varibles _URL_NEWS y _APIKEY

--- lib/src/services/news_sercive.dart
+++ lib/src/services/news_sercive.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notificaciones/src/models/news_models.dart';
 
-
+final _URL_NEWS = "https://newsapi.org/v2";
+final _APIKEY = "3739e2bda7cf4d99b4a1a6df98e846a3";
 
 class NewsService with ChangeNotifier{
 
@@ -15,7 +16,9 @@ class NewsService with ChangeNotifier{
 
 
   getTopHeadlines(){
-    print('Cargando Headlines');
+    
+    final url = "/top-headlines?country=ca&apiKey=3739e2bda7cf4d99b4a1a6df98e846a3";
+
   }
 
 
