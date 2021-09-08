dcb02ca9362ff405c32c83440af453faee485102
Jair <jairaceves56@gmail.com>
2021-09-08 11:01:03 -0500

    Creamos una instancia de Provider del NewsService

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
 
 import 'package:flutter/material.dart';
 import 'package:notificaciones/src/pages/tabs_page.dart';
+import 'package:notificaciones/src/services/news_sercive.dart';
 import 'package:notificaciones/src/theme/tema.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: miTema,
-      title: 'Material App',
-      home: TabsPage(),
-      
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create:(_) => new  NewsService() )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: miTema,
+        title: 'Material App',
+        home: TabsPage(),
+        
+      ),
     );
   }
 }
